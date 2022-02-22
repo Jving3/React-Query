61f66eef767946a6f26c39f12cacc17a774d994f
Jorge Luis Vera <veravelis3@gmail.com>
2022-02-22 15:31:46 -0300

    Formulas resueltas y grafico funcionando

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import HomePage from './components/Home.page';
-import KpiPage from './components/Kpi.page';
 import RQKpiPage from './components/RQKpi.page';
 import './index.css';
 
@@ -19,16 +18,12 @@ function App() {
                       <Link to='/'>Home</Link>
                     </li>
                     <li>
-                      <Link to='/kpi'>Kpi Page</Link>
-                    </li>
-                    <li>
                       <Link to='/rq-kpi'>RQ Kpi Page</Link>
                     </li>
                   </ul>
                 </nav>
                 <Routes>
                   <Route path='/' element={<HomePage/>}/>
-                  <Route path='/kpi' element={<KpiPage/>}/>
                   <Route path='/rq-kpi' element={<RQKpiPage/>}/>   
                 </Routes>
               </div>
